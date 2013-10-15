5618d0da9bf50f29ef4fa2d396dc07c38b555d48
Mion Giovanni <mion.ggb@gmail.com>
2013-10-15 22:46:16 +0200

    Mise en forme
    Désolé les gas pour la redondance mais il fallait que je corrige un
    problème d'espaces et de tabulations lool

--- Gio/TP1/colimacon.c
+++ Gio/TP1/colimacon.c
@@ -1,70 +1,71 @@
 /**********************************************************************/
 /*                                                                    */
-/*                  TP1 - COLIMACON - MION GIOVANNI                   */
+/*                  TP1 - COLIMACON.C - MION GIOVANNI                 */
 /*                                                                    */
 /**********************************************************************/
 #include <stdlib.h>
 #include <stdio.h>
 
 int colimacon(int **array, unsigned int rows, unsigned int columns){
-    
-  //definition des variables utilisees par l'algorithme
-  unsigned int r_max,r_min,c_max,c_min,cpt,cpt_max;
-  int** tmp;
-  
-  //definition des varibles de boucle
-  int i;
-  
-  //creation du tableau
-  tmp = (int**)malloc(rows*sizeof(int*));
-  if(tmp==NULL)
+
+	//definition des variables utilisees par l'algorithme
+	unsigned int r_max,r_min,c_max,c_min,cpt,cpt_max;
+	int** tmp;
+
+	//definition des varibles de boucle
+	int i;
+
+	//creation du tableau
+	tmp = (int**)malloc(rows*sizeof(int*));
+	if(tmp==NULL)
 	return -1;
-  for(i=0;i<rows;i++){
-    tmp[i]=(int*)malloc(columns*sizeof(int));
-    if(tmp[i]==NULL)
-		return -1;
+	for(i=0;i<rows;i++){
+			tmp[i]=(int*)malloc(columns*sizeof(int));
+	    if(tmp[i]==NULL)
+			return -1;
   } 
 
-  //initialisation des variables de l'algorithme
-  r_max = rows-1;
-  r_min = 0;
-  c_max = columns-1;
-  c_min = 0;
-  cpt = 1;
-  cpt_max = rows * columns;
-  
-  // debut de l'algorithme de remplissage
-  while(cpt <= cpt_max ){
+	//initialisation des variables de l'algorithme
+	r_max = rows-1;
+	r_min = 0;
+	c_max = columns-1;
+	c_min = 0;
+	cpt = 1;
+	cpt_max = rows * columns;
 
-    for(i=c_min;i<=c_max;i++){
-      tmp[r_min][i]=cpt;
-      cpt++;
-    }
-    r_min++;
+	// debut de l'algorithme de remplissage
+	// on remplis les ligne et colonnes les plus extérieurs qui ne sont pas encore remplie (1 boucle "for" par bords)
+	while(cpt <= cpt_max ){
 
-    for(i=r_min;i<=r_max;i++){
-      tmp[i][c_max]=cpt;
-      cpt++;
-    }
-    c_max--;
-    
-    for(i=c_max;(i>=(int)c_min)&&((int)r_max>=(int)r_min);i--){
-      tmp[r_max][i]=cpt;
-      cpt++;
-    }
-    r_max--;
-	
-    for(i=r_max;(i>=(int)r_min)&&((int)c_max>=(int)c_min);i--){
-      tmp[i][c_min]=cpt;
-      cpt++;
-    }
-    c_min++;
+			for(i=c_min;i<=c_max;i++){
+				tmp[r_min][i]=cpt;
+				cpt++;
+			}
+			r_min++;
 
-  }
-  *(array)=(int*)&(tmp[0]);
-  if(array==NULL)
+			for(i=r_min;i<=r_max;i++){
+				tmp[i][c_max]=cpt;
+				cpt++;
+			}
+			c_max--;
+
+			for(i=c_max;(i>=(int)c_min)&&((int)r_max>=(int)r_min);i--){
+				tmp[r_max][i]=cpt;
+				cpt++;
+			}
+			r_max--;
+
+			for(i=r_max;(i>=(int)r_min)&&((int)c_max>=(int)c_min);i--){
+				tmp[i][c_min]=cpt;
+				cpt++;
+			}
+			c_min++;
+
+	}
+	*(array)=(int*)&(tmp[0]);
+	if(array==NULL)
 	return -1;
-	
-  return 0;
+
+	return 0;
 
 }
--- Gio/TP1/main.c
+++ Gio/TP1/main.c
@@ -1,6 +1,6 @@
 /**********************************************************************/
 /*                                                                    */
-/*                  TP1 - COLIMACON - MION GIOVANNI                   */
+/*                  TP1 - MAIN.C - MION GIOVANNI                      */
 /*                                                                    */
 /**********************************************************************/
 
@@ -9,50 +9,52 @@
 #include "libcolimacon.h"
 
 int main(int argc,char **argv){
-  if(argc<3){
+	if(argc<3){
+			printf("Erreur: veuillez passer en argument au programme le nombre de lignes et le nombre de colonnes\n");
+			return -1;
 
-    printf("Erreur: veuillez passer en argument au programme le nombre de lignes et le nombre de colonnes\n");
-    return -1;
+	}else{
+  
+			// definition des variables
+			unsigned int rows,columns;
+			int val_ret,i,j;
+			int *array;
+			int *p;
 
-  }else{
-    
-    // definition des variables
-    unsigned int rows,columns;
-    int val_ret,i,j;
-    int *array;
-	int *p;
+			//lecture des arguments et traitement des cas negatifs
+			rows = (unsigned int)atoi(argv[1]);
+			columns = (unsigned int) atoi(argv[2]);
+			if(rows <=0 || columns <=0){
+				printf("Les valeurs du nombre de colonnes et/ou de lignes est invalide\n");
+				return -1;
+			}
 
-    rows = (unsigned int)atoi(argv[1]);
-    columns = (unsigned int) atoi(argv[2]);
-    if(rows <=0 || columns <=0){
-      printf("Les valeurs du nombre de colonnes et/ou de lignes est invalide\n");
-      return -1;
+			//Creation du tableu par la librairie colimacon
+			printf("Creation du tableau en colimacon de %d ligne et %d colonnes\n",rows,columns);
+			val_ret = colimacon(&array,rows,columns);
+			printf("Affichage du tableau\n\n");
+			if(val_ret<0){
+				//Erreur lors du traitement de colimacon
+				printf("Erreur, la fonction colimacon a rencontrer un probleme\n");
+				return -1;       
+
+			}else{
+
+				// affichage des elements du tableau
+				for(i=0;i<rows;i++){
+					printf("[");
+					for(j=0;j<columns;j++){
+						//Calcule de l'addresse de l'éléments a la ligne i et la colonne j et affichage de l'element
+						p=(int*) (*(*(&array)+i)+j*sizeof(int));
+						printf("%d; ",*p);
+        	}
+					printf("]\n");
+				}
+				printf("fin\n");
     }
-    
-    printf("Creation du tableau en colimacon de %d ligne et %d colonnes\n",rows,columns);
-    val_ret = colimacon(&array,rows,columns);
-    printf("Affichage du tableau\n\n");
-    if(val_ret<0){
 
-      printf("Erreur, la fonction colimacon a rencontrer un probleme\n");
-      return -1;       
-
-    }else{
-      for(i=0;i<rows;i++){
-        printf("[");
-        for(j=0;j<columns;j++){
-		  p=(int*) (*(*(&array)+i)+j*sizeof(int));
-          printf("%d; ",*p);
-        }
-        printf("]\n");
-      }
-      printf("fin\n");
-	
-    }
-
-    return 0;
+		return 0;
 
   }
-
 
 }
