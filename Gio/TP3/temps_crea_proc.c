28769e5ff1d91f4121b7ecab7cfe437bb5f73525
Mion Giovanni <mion.ggb@gmail.com>
2013-11-12 22:42:26 +0100

    TP3 - ajout des tmps RT & CPU pour les proc

--- Gio/TP3/temps_crea_proc.c
+++ Gio/TP3/temps_crea_proc.c
@@ -3,6 +3,8 @@
 /*                  TP3 - TEMPS_CREA_PROC.C - MION GIOVANNI           */
 /*                                                                    */
 /**********************************************************************/
+  // NOTE : lancer le programme avec taskset -c 0 ./temps_crea_thread
+
 
 #include <time.h>
 #include <math.h>
@@ -12,7 +14,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-double array_moy(long* array,int length){
+double array_moy(double* array,int length){
   //decalration des variables
   double cpt;
   int ind;
@@ -29,18 +31,24 @@ double array_moy(long* array,int length){
 int main(int argc,char** argv){
 
   //declaration des variables
-  struct timespec t_start,t_stop;
-  long mem_creations[50];
-  double moy;
+	struct timespec t_start_rt,t_stop_rt,t_start_cpu,t_stop_cpu,cpu_res,rt_res;
+  double mem_creations_rt[50],mem_creations_cpu[50];
+  double moy_rt,moy_cpu;
   int i,j,ret_fork;
   
+  // on regarde la resolution des mesures
+  clock_getres(CLOCK_REALTIME,&rt_res);
+  clock_getres(CLOCK_PROCESS_CPUTIME_ID,&cpu_res);
+
   //mesure du temps de creation d'un processus
   
   //on fait cette mesure sur 50 cretaions de 50 processus et on calcule la moyenne
   for(i=0;i<50;i++){
     
     //prise du temps avant creation.
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t_start);
+		clock_gettime(CLOCK_REALTIME, &t_start_rt);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t_start_cpu);
+
     for(j=0;j<50;j++){
       //creation du fils
       ret_fork = fork();
@@ -52,14 +60,20 @@ int main(int argc,char** argv){
       //dans le pere on attend la fin du fils et l'on recupere le temps
     for(j=0;j<50;j++)
       wait(NULL);
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t_stop);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t_stop_cpu);
+    clock_gettime(CLOCK_REALTIME, &t_stop_rt);
     //on calcule le temps reel passer entre les deux temps et on la stock dans mem_creations
-    mem_creations[i]= ( ((t_stop.tv_sec*pow(10,9)+t_stop.tv_nsec) - (t_start.tv_sec*pow(10,9)+t_start.tv_nsec))/50 );
+    mem_creations_cpu[i]= ( ((t_stop_cpu.tv_sec*pow(10,9)+t_stop_cpu.tv_nsec) - (t_start_cpu.tv_sec*pow(10,9)+t_start_cpu.tv_nsec))/50.0 );
+    mem_creations_rt[i]= ( ((t_stop_rt.tv_sec*pow(10,9)+t_stop_rt.tv_nsec) - (t_start_rt.tv_sec*pow(10,9)+t_start_rt.tv_nsec))/50.0 );
   }
   
   //calule de la moyenne et affichage de celle-ci
-  moy = array_moy(mem_creations,50);
-  printf("la moyenne de temps pour la creation/destruction de processus est:\n%f nanosecondes\n%f secondes\n",moy,moy/pow(10,9));
+  moy_cpu = array_moy(mem_creations_cpu,50);
+  moy_rt = array_moy(mem_creations_rt,50);
+  printf("\nla moyenne de temps CPU pour la creation/destruction de processus est:\n%f nanosecondes\n%f secondes\n\n",moy_cpu,moy_cpu/pow(10,9));
+  printf("la moyenne de temps RT pour la creation/destruction de processus est:\n%f nanosecondes\n%f secondes\n\n",moy_rt,moy_rt/pow(10,9));
+
+  printf("resolution des mesures:\nCPU = %ld nsec\nRT = %ld nsec\n\n",cpu_res.tv_nsec,rt_res.tv_nsec);
 
 
   return 1;
